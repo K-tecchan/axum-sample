0a8c7549a857d451a224e06d0e9736afeb9a7262
k-tecchan <tecchanboy+pc@gmail.com>
2025-01-11 23:45:47 +0900

    パスパラメータやってみた

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::{
+    extract::{Path, RawPathParams},
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
@@ -11,7 +12,11 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     let app = Router::new()
-        .route("/", get(root))
+        .route("/", get(|| async { "hogehoge" }))
+        .route(
+            "/hoge/{name}",
+            get(|Path(name): Path<String>| async move { format!("name = {}", name) }),
+        )
         .route("/users", post(create_user));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
@@ -20,10 +25,6 @@ async fn main() {
 
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
-}
-
-async fn root() -> &'static str {
-    "Hello, World!"
 }
 
 async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
